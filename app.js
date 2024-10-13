070ee53e2ec458877b7f580ad1012ec8a21b41e3
sudhanshu <sudhanshumishra9491@gmail.com>
2024-10-13 19:18:56 +0530

    removed some extra space from app.js

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
-    const express=require('express');
-    const app = express();
-    const mongoose =require('mongoose');
-    const path=require('path');
-    const methodOverride=require('method-override');
-    const ejsMate=require('ejs-mate');
-    const ExpressError=require("./utils/ExpressError.js");
+const express=require('express');
+const app = express();
+const mongoose =require('mongoose');
+const path=require('path');
+const methodOverride=require('method-override');
+const ejsMate=require('ejs-mate');
+const ExpressError=require("./utils/ExpressError.js");
 
-    // Router 
-    const listings=require("./routes/listing.js")
-    const reviews=require("./routes/review.js")
+// Router 
+const listings=require("./routes/listing.js")
+const reviews=require("./routes/review.js")
 
-    // Url is taken from mongodb website -->/wanderlust is a project name
-    const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+// Url is taken from mongodb website -->/wanderlust is a project name
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
-    // set up of mongo db 
-    async function main(){
-        await mongoose.connect(MONGO_URL);
-    }
+// set up of mongo db 
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
 
-    // Execution of main 
-    main().then(()=>{
-        console.log("Connected to DB");
-    }).catch((err)=>{
-        console.error(err);
-    });
+// Execution of main 
+main().then(()=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.error(err);
+});
 
-    app.set("view engine",'ejs');
-    app.set("views",path.join(__dirname,"views"));
-    app.use(express.urlencoded({extended:true}));
-    app.use(methodOverride("_method"));
-    app.engine("ejs",ejsMate);
-    app.use(express.static(path.join(__dirname,"/public")));
-  
-
-    // Router routes
-    app.use("/listings",listings);
-    app.use("/listings/:id/review",reviews);
-    
-
-    // Root of the server
-    app.get("/",(req,res)=>{
-        res.render("./listings/home.ejs");
-        // res.send("This is the set Up of App.js");
-    })
+app.set("view engine",'ejs');
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
-    // if any page is not found then this will get the request
-    app.all("*", (req, res, next) => {
-      next(new ExpressError(404, "page not found"));
-    });
+// Router routes
+app.use("/listings",listings);
+app.use("/listings/:id/review",reviews);
 
-    // For all universal err
-    app.use((err,req,res,next)=>{
-        let {statusCode=500,message="Something went wrong"}=err;
-        res.status(statusCode).render("./listings/error.ejs", { message });
-        // res.status(statusCode).send(message);
-    });
 
-    // listenig app
-    app.listen(8080,()=>{
-        console.log("Server is started");
-    })
+// Root of the server
+app.get("/",(req,res)=>{
+    res.render("./listings/home.ejs");
+    // res.send("This is the set Up of App.js");
+})
+
+
+// if any page is not found then this will get the request
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "page not found"));
+});
+
+// For all universal err
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something went wrong"}=err;
+    res.status(statusCode).render("./listings/error.ejs", { message });
+    // res.status(statusCode).send(message);
+});
+
+// listenig app
+app.listen(8080,()=>{
+    console.log("Server is started");
+})
