ca5f3b48d6e77f5f21ee4cbe7de15f89b9dbc3da
sudhanshu <sudhanshumishra9491@gmail.com>
2024-10-14 19:49:02 +0530

    httpOnly:true for scripting attack

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const sessionOptions = {
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000 ,
-        httpOnly:true
+        httpOnly:true // for scripting attack
     }
 }
 
