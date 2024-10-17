61268b8546e28cc1051c28394ffc3a32bee77be5
sudhanshu <sudhanshumishra9491@gmail.com>
2024-10-17 09:58:49 +0530

    we added a logged in as a middele ware in update,add,new,edit listing

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const {listingSchema} =require("../schema.js");
 const listing =require('../models/listing.js');
-
+const {isLoggedIn} =require("../middleware.js");
 
 const validateListing=(req,res,next)=>{
     let {error} =listingSchema.validate(req.body);
@@ -25,11 +25,8 @@ router.get("/",wrapAsync(async (req,res)=>{
 
 // we write this before the listings/:id because the listings is same so it is searching the id in the database
 // Created new listing
-router.get("/new",(req,res)=>{
-    if(!req.isAuthenticated()){
-        req.flash("error","You must be logged in to create new listing!");
-        return res.redirect("/login");
-    }
+router.get("/new",isLoggedIn,(req,res)=>{
+    console.log(req.user);
     res.render("./listings/new.ejs");
 })
 
@@ -47,7 +44,7 @@ router.get("/:id",wrapAsync(async (req,res,next)=>{
     // res.send(";dhja");
 }))
 
-router.post("/",validateListing,wrapAsync(async (req,res,next)=>{
+router.post("/",isLoggedIn,validateListing,wrapAsync(async (req,res,next)=>{
     const newListing=new listing(req.body.listing);
     await newListing.save();
     req.flash("success","New Listing Created !");
@@ -55,7 +52,7 @@ router.post("/",validateListing,wrapAsync(async (req,res,next)=>{
 }))
 
  // Update route
- router.put("/:id",validateListing,wrapAsync(async (req,res)=>{
+ router.put("/:id",isLoggedIn,validateListing,wrapAsync(async (req,res)=>{
     // if nothing will come via hoppscotch then
     let {id}=req.params;
     await listing.findByIdAndUpdate(id,{...req.body.listing});
@@ -64,7 +61,7 @@ router.post("/",validateListing,wrapAsync(async (req,res,next)=>{
 }))
 
 // Edit Route
-router.get("/:id/edit",wrapAsync(async (req,res)=>{
+router.get("/:id/edit",isLoggedIn,wrapAsync(async (req,res)=>{
     let {id} =req.params;
     let data=await listing.findById(id);
     if(!data){
@@ -76,7 +73,7 @@ router.get("/:id/edit",wrapAsync(async (req,res)=>{
 }))
 
 // Delete
-router.get("/:id/delete",wrapAsync(async (req,res)=>{
+router.get("/:id/delete",isLoggedIn,wrapAsync(async (req,res)=>{
     let {id} =req.params;
     console.log(id);
     await listing.findByIdAndDelete(id);
