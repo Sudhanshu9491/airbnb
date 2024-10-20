7c2e17b3017478273a80af9b5597d0dca8bf6eae
sudhanshu <sudhanshumishra9491@gmail.com>
2024-10-20 11:04:26 +0530

    controller for review to separate teh backend logic

--- routes/review.js
+++ routes/review.js
@@ -5,70 +5,14 @@ const ExpressError = require("../utils/ExpressError.js");
 const listing = require('../models/listing.js');
 const Review = require('../models/review.js');
 const {validateReview,isLoggedIn,isReviewAuthor} =require("../middleware.js");
+const  reviewController = require("../controllers/reviews.js");
 
 
 // Reviews 
 // Post route
-router.post("/",isLoggedIn, validateReview, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    if (!id) {
-        throw new ExpressError(404, "Listing ID is required");
-    }
-
-    const Listing = await listing.findById(id);  
-    if (!Listing) {
-        throw new ExpressError(404, "Listing not found");
-    }
-    
-    const { review } = req.body;
-    if (!review) {
-        throw new ExpressError(400, "Review is required");
-    }
-
-    const newReview = new Review(review);
-    newReview.author=req.user._id;
-    // console.log("something about user"+req.user._id);
-    Listing.reviews.push(newReview);  
-    
-    try {
-        await newReview.save();
-        await Listing.save();
-    } catch (err) {
-        throw new ExpressError(500, "Error saving review");
-    }
-    
-    console.log("new review saved");
-    req.flash("success","New Review Added Successful!");
-    res.redirect(`/listings/${Listing._id}`);
-}));
+router.post("/",isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
 
 // Delete route with list.id and reviews.id
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    if (!id || !reviewId) {
-        throw new ExpressError(404, "Listing ID and Review ID are required");
-    }
-
-    try {
-        const Listing = await listing.findById(id);
-        if (!Listing) {
-            throw new ExpressError(404, "Listing not found");
-        }
-
-        const reviewIndex = Listing.reviews.findIndex((review) => review._id.toString() === reviewId);
-        if (reviewIndex === -1) {
-            throw new ExpressError(404, "Review not found");
-        }
-
-        Listing.reviews.splice(reviewIndex, 1);
-        await Listing.save();
-
-        await Review.findByIdAndDelete(reviewId);
-    } catch (err) {
-        throw new ExpressError(500, "Error deleting review");
-    }
-    req.flash("success","Review Is Deleted Successful!");
-    res.redirect(`/listings/${id}`);  
-}));
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.deleteReview));
 
 module.exports = router;
