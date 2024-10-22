e5d11a6cbe335936cacc31dd01ad0d7d0a3f6c2c
sudhanshu <sudhanshumishra9491@gmail.com>
2024-10-23 00:00:59 +0530

    .

--- routes/listing.js
+++ routes/listing.js
@@ -1,37 +1,45 @@
-const express  = require('express');
-const router= express.Router();
-const wrapAsync=require("../utils/wrapAsync.js");
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
 // const listing =require('../models/listing.js');
-const {isLoggedIn, isOwner, validateListing} =require("../middleware.js");
-const listingController  = require("../controllers/listings.js");
-
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
 
 // All data show in all listining
-router.route("/")
-.get(wrapAsync(listingController.index))
-.post(isLoggedIn,validateListing,wrapAsync(listingController.newListing));
-
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedIn, validateListing, wrapAsync(listingController.newListing));
 
 // we write this before the listings/:id because the listings is same so it is searching the id in the database
 // Created new listing
-router.get("/new",isLoggedIn,(req,res)=>{
-    console.log(req.user);
-    res.render("./listings/new.ejs");
-})
+router.get("/new", isLoggedIn, (req, res) => {
+  console.log(req.user);
+  res.render("./listings/new.ejs");
+});
 
 // show
 // Inside of any listed item view
-router.route("/:id")
-.get(wrapAsync(listingController.show))
-.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.update))
-.delete(isLoggedIn,isOwner,wrapAsync(listingController.delete));
-
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.show))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.update)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.delete));
 
 // Edit Route
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.edit))
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.edit));
 
 // Delete
-router.get("/:id/delete",isLoggedIn,isOwner,wrapAsync(listingController.delete))
-
+router.get(
+  "/:id/delete",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.delete)
+);
 
 module.exports = router;
