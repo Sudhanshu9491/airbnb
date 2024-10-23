b754f559629f9d7cba11d7e80ae123d0a9b1ac04
sudhanshu <sudhanshumishra9491@gmail.com>
2024-10-24 00:21:01 +0530

    set up cloudinary and upload file setup and access from cloud

--- controllers/listings.js
+++ controllers/listings.js
@@ -18,8 +18,12 @@ module.exports.show=async (req,res,next)=>{
 };
 
 module.exports.newListing=async (req,res,next)=>{
+    let url=req.file.path;
+    let filename=req.file.filename;
+    console.log(url," . ",filename);
     const newListing=new listing(req.body.listing);
     newListing.owner=req.user._id;
+    newListing.image={url,filename};
     await newListing.save();
     req.flash("success","New Listing Created !");
     res.redirect("/listings");
